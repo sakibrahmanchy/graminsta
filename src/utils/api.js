0dc2286810df6f4ac05266d68f442da38302e694
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2020-08-05 05:37:19 +0600

    Built at 5 august, 2020 5.19 am

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,5 +3,5 @@
  * @type {string}
  */
 // export const baseUrl = 'http://localhost:8078';
-export const baseUrl = 'http://ec2-13-127-125-228.ap-south-1.compute.amazonaws.com:8078';
+export const baseUrl = '/api';
 
