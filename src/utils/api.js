ff614028b6e8395f2b7b30cde8c7ca62fad8a34e
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2020-08-05 06:53:44 +0600

    Built at 5 august, 2020 5.19 am

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
  * Base url to hit
  * @type {string}
  */
-// export const baseUrl = 'http://localhost:8078';
-// export const baseUrl = '52.66.251.101:8078';
+// export const baseUrl = 'http://localhost:80';
+// export const baseUrl = '52.66.251.101:80';
 export const baseUrl = '/api';
 
