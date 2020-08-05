21457bd286a6e788423d24f79fba9d6508b81456
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2020-08-05 14:54:12 +0600

    Build at 5th august, 2020 2:54pm

---
+++ build/precache-manifest.ff920b1b2d7db75118a0d3adb4c8be81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8580bb6146b9543c7599cd32f2197fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b4f11a841307681068",
+    "url": "/static/css/main.dc89bf4b.chunk.css"
+  },
+  {
+    "revision": "bd085c414e6e5f91a0d6",
+    "url": "/static/js/2.04f8e1b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.04f8e1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b4f11a841307681068",
+    "url": "/static/js/main.48aa98fe.chunk.js"
+  },
+  {
+    "revision": "5a2b24d1f5583b3eebdd",
+    "url": "/static/js/runtime-main.c582cd99.js"
+  },
+  {
+    "revision": "a84414a6d98daa4208228d6e1bc94aa6",
+    "url": "/static/media/login.a84414a6.jpeg"
+  }
+]);
