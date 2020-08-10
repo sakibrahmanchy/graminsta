57aacf2c44a0655c58d17a62b19c13660a223331
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2020-08-11 04:32:37 +0600

    Build front end at 11 August,2020 @ 4.23am

---
+++ build/precache-manifest.e1ec05b69b2c6dfe5af09d088ebf6f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d92bd655cb9046da52ace2609b6c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed290b1d8bc6f35fcf7f",
+    "url": "/static/css/main.dc89bf4b.chunk.css"
+  },
+  {
+    "revision": "bd085c414e6e5f91a0d6",
+    "url": "/static/js/2.04f8e1b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.04f8e1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed290b1d8bc6f35fcf7f",
+    "url": "/static/js/main.89f25710.chunk.js"
+  },
+  {
+    "revision": "5a2b24d1f5583b3eebdd",
+    "url": "/static/js/runtime-main.c582cd99.js"
+  },
+  {
+    "revision": "a84414a6d98daa4208228d6e1bc94aa6",
+    "url": "/static/media/login.a84414a6.jpeg"
+  }
+]);
